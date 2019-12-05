e8d17ba0145b3cc5f9b25a76de8aafda53a67e48
leaelahdab <lea.el-ahdab@toulouse.miage.fr>
2019-12-05 13:54:50 +0100

    Gitignore + add files

---
+++ src/main/java/miage/lela/bowling/App.java
@@ -0,0 +1,12 @@
+package miage.lela.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
